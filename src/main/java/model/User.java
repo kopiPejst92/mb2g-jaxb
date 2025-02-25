15192036f28274436d4919db41d745515b9b2b2a
kopiPejst92 <agkop01@gmail.com>
2025-02-25 11:51:12 +0100

    adding lombok generation of object and builder of User

---
+++ src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName, lastName, eMail;
+}
